d2f12fd2165f2397f9698bf0bb98e82bf779c143
github username <github email address>
2019-12-08 16:41:35 -0800

    Rename Navbar to NavigationBar

---
+++ src/containers/NavigationBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+
+function NavigationBar() {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Nav className='mr-auto'>
+          <Navbar.Brand href='/'>A Stardust Truce</Navbar.Brand>
+          <NavLink to='/' className="nav-link">Home</NavLink>
+          <NavLink to='/structures' className="nav-link">Structures</NavLink>
+          <NavLink to='/login' className="nav-link">Login</NavLink>
+        </Nav>
+      </Navbar>
+    </div>
+  )
+}
+
+export default NavigationBar;
