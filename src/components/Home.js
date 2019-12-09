0dfa4a2ed757c98f02593b6a94808091e40d1c2f
github username <github email address>
2019-12-08 16:41:57 -0800

    Add components folder

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div className='home-header'><b>A Stardust Truce!</b></div>
+      <div className='home-sub-header'><em>Guiding you to a Better Understanding of Data Structures</em></div>
+    </div>
+  )
+}
+
+export default Home;
