e1a3505ded6b56242197e0f775827a0c32af69e5
Elaine Laguerta <elaine.laguerta@gmail.com>
2019-12-20 17:51:34 -0800

    Setup npm for testing

---
+++ test/test.js
@@ -0,0 +1,8 @@
+// import assert, a Node module useful for testing
+const assert = require('assert');
+
+// Write all tests using the 4 phase approach: setup, exercise, verify, teardown.
+// 1. Setup: create objects, variables, and set conditions that the test depends on
+// 2. Exercise: execute the functionality you're testing
+// 3. Verify: check your expectations against the result of the exercise phase. 
+// 4. Teardown: (optional) reset any conditions that were changed during test. 
